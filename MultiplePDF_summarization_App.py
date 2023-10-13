3a0d72ec22458e032fcb3d824daea5e5e31ee957
Naveen Kumar T <144416721+naveen-ktn@users.noreply.github.com>
2023-10-13 13:28:03 +0530

    Update MultiplePDF_summarization_App.py

--- MultiplePDF_summarization_App.py
+++ MultiplePDF_summarization_App.py
@@ -8,7 +8,7 @@ import tempfile
 import os
 
 # Set up HuggingFace API
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
 
 llm = HuggingFaceHub(
     repo_id='meta-llama/Llama-2-70b-chat-hf', model_kwargs={"temperature": 0.5, "max_length": 16384}
